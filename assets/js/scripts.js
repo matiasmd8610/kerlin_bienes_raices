c35d55d9deb7ea90d73d4176735210be14c41352
Lucas Garcia Buixens <Lucasgb@hotmail.com>
2023-02-08 17:05:27 -0300

    agregado Swiper basico con 3 fotos

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,28 @@
-//Update copy automatically
+// Swiper initialize //
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+  //End swiper//
+
+  //Update copy automatically
 document.querySelector(".year-copy").innerText = new Date().getFullYear();
