50bef33698e3455d495de7fb232a4b87c388a2ca
Matias Di Iorio <matiasmd8610@gmail.com>
2023-03-06 19:00:30 -0300

    Updates

--- phpmailer.php
+++ phpmailer.php
@@ -15,16 +15,16 @@ $name = $_POST['name'];
 $telephone = $_POST['telephone'];
 $email = $_POST['email'];
 $message = $_POST['message'];
-$subject = 'Mensaje recibido desde lucioteposiciona.com';
+$subject = 'Mensaje recibido desde www.loteoskerlin.com.ar';
 
-$recaptcha_secret = "xxxxxxxxxxxxxxxxxxxxxxxx"; //Add secret key
+$recaptcha_secret = "6Ld8e8wkAAAAAEdq6opRmM9RV-qmVoqAnVYerI0U"; //Add secret key
 $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$recaptcha_secret."&response=".$_POST['g-recaptcha-response']);
 $response = json_decode($response, true);
 
 if( empty(trim($name)) ) $name = 'anonimo';
 
 $body = <<<HTML
-    <h1>Mensaje recibido desde loteskerlin.com.ar</h1>
+    <h1>Mensaje recibido desde www.loteoskerlin.com.ar</h1>
     <p>De: $name | $email | $telephone</p>
     <!-- <h2>Mensaje:</h2> -->
     $message
@@ -36,10 +36,10 @@ try {
     //Server setting
     $mailer->SMTPDebug = 0;
     $mailer->isSMTP();
-    $mailer->Host = 'c1591626.ferozo.com';
+    $mailer->Host = 'c1561910.ferozo.com';
     $mailer->SMTPAuth = true;  
-    $mailer->Username = 'no-reply@c1591626.ferozo.com';
-    $mailer->Password = 'jr@gQXC3wG';                          
+    $mailer->Username = 'no-reply@c1561910.ferozo.com';
+    $mailer->Password = 'wpFD@hW1SB';                          
     $mailer->SMTPSecure = 'ssl';
     $mailer->Port = 465;
     //$mailer->AltBody = strip_tags($body);
@@ -47,7 +47,7 @@ try {
 
     //Recipients
     $mailer->setFrom( $email, "$name" );
-    $mailer->addAddress('no-reply@c1591626.ferozo.com','Sitio web');
+    $mailer->addAddress('no-reply@c1561910.ferozo.com','Sitio web');
 
     //Content
     $mailer->isHTML(true);
